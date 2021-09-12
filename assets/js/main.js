bf73642a4ca8e287252de60f52675b41d52f000a
Alexander <aldonald@davidson.edu>
2021-09-12 11:53:20 -0400

    final iteration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 
 /* javascript */
 
+document.addEventListener("mousemove", () => {
+  let mousex = event.clientX; // Gets Mouse X
+  let mousey = event.clientY; // Gets Mouse Y
+  //console.log([mousex, mousey]); // Prints data
+  document.getElementById("mouse").innerHTML = "X: " + mousex + ", " + "Y: " + mousey;
+});
+
+var total_elements = document.querySelectorAll("div").length;
+console.log("Total elements in self portrait: " + total_elements);
+
 var bauhaus = `
 
 Staaatliches Bauhaus (1919-1933)
